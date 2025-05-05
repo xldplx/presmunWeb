020b67f510d5603331e689befc4896da1aa5f818
matt <aldraddd01@gmail.com>
2025-05-05 14:05:20 +0700

    server template remake, new vite and tailwindcss version

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/presmunWeb/",
-  build: {
-    assetsInclude: ['**/*.otf', '**/*.ttf'],
-  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
